7e604187c8937a4bb07097a63f2ed70602d8ce24
Mitranjay Krishna <mitranjaykrishna@gmail.com>
2023-04-20 20:24:07 +0530

    Home edit 2

--- src/componenst/HeroSection.js
+++ src/componenst/HeroSection.js
@@ -33,7 +33,88 @@ const HeroSection=()=>{
 
 const Wrapper= styled.section `
 
-    
+    padding: 9rem 0;
+
+    .section-hero-data{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        
+    }
+
+    .hero-heading img{
+        height: 15rem;
+        width: 100%
+    }
+
+    .btn{
+        max-width: 16rem;
+    }
+
+    .logReg{
+        
+        font-size: 1.5rem;
+    }
+
+    .buttonEffect :hover{
+    color: rgb(71, 209, 240);
+    text-decoration: underline;
+    transform: scale(0.96);
+    transition: 0.3s ease-in-out;
+    }
+
+
+    .hero-top-data{
+        text-transform: uppercase;
+        font-weight: 500;
+        font-size: 2.5rem;
+        color: ${({theme})=> theme.colors.helper};
+    }
+
+    .hero-para {
+    margin-top: 1.5rem;
+    margin-bottom: 3.4rem;
+    max-width: 60rem;
+    text-align: justify;
+    text-justify: inter-character;
+    }
+    .button2{
+            color: white;
+        }
+
+    .section-hero-image{
+        display:flex;
+        justify-content: center;
+        max-width:60rem;
+        
+    }
+
+    picture {
+        text-align: center;
+    }
+
+    .hero-img {
+        max-width: 80%;
+    }
+
+    .fluidAnim{
+        animation: MoveUpDown 8s ease-in-out infinite alternate-reverse both;
+    }
+
+    @keyframes MoveUpDown {
+    0%, 100% {
+        transform: translateY(0);
+    }
+    50% {
+        transform: translateY(30px);
+    }
+}
+
+    @media (max-width: ${({ theme }) => theme.media.mobile}) {
+        .grid {
+        gap: 7.2rem;
+        }
+    }
 `;
 
 export default HeroSection;
